ed7647eb57de0b707b4bcb14ff27c1f4a7edceb9
gooseknight <137021325+CopperTrin@users.noreply.github.com>
2025-09-14 00:00:28 +0700

    Add color by palette.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: {
+          100: '#211A3A',
+          200: '#140E25',
+        },
+        secondary: {
+          100: '#BE3DFC',
+          200: '#7431FA',
+        },
+        accent: {
+          100: '#F6F683',
+          200: '#FFD824',
+        },
+        blackpearl: '#0D1C2B',
+        alabaster: '#F8F8F8',
+        
+      }
+    },
   },
   plugins: [],
 }
