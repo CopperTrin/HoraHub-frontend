d92b953d2de07be4c4ef53bb0143d36e1c6e7a22
BrIghTbright007 <iambright2547@hotmail.com>
2025-09-20 00:12:48 +0700

    add top nav bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-import { FontAwesome, MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
 import '../global.css';
@@ -79,44 +79,5 @@ export default function TabLayout() {
         }}
       />
     </Tabs>
-  );
-}
-
-export default function TabsLayout() {
-  return (
-  <Tabs 
-  screenOptions={{
-    tabBarActiveTintColor: '#ffd824',
-    tabBarStyle: { backgroundColor: '#211a3a', height: 60, paddingBottom: 4, paddingTop: 4 },
-    headerStyle: { backgroundColor: '#140E25'},
-    tabBarPosition: 'bottom',
-    headerTitleStyle: { color: '#fff', fontSize: 20, fontWeight: 'bold'}
-  }}
-  >
-    <Tabs.Screen name="Home"
-    options={{ headerTitle : "Home",
-      tabBarIcon: ({ color, size }) => (
-        <FontAwesome name="home" color={color} size={size} />
-      ),
-    }}/>
-    <Tabs.Screen name="P2P"
-    options={{ headerTitle : "P2P",
-      tabBarIcon: ({ color, size }) => (
-        <FontAwesome name="users" color={color} size={size} />
-      ),
-    }} />
-    <Tabs.Screen name="Shop"
-    options={{ headerTitle : "Shop",
-      tabBarIcon: ({ color, size }) => (
-        <FontAwesome name="shopping-bag" color={color} size={size} />
-      ),
-    }} />
-    <Tabs.Screen name="Notification" 
-    options={{ headerTitle : "Notification",
-      tabBarIcon: ({ color, size }) => (
-        <FontAwesome name="bell" color={color} size={size} />
-      ),
-    }} />
-  </Tabs>
   );
 }
