19b31bb0bc82392a29e3837cada81425328a49b7
Siasa <siasa18037@gmail.com>
2025-09-26 13:59:01 +0700

    create shop dashbroad

--- app/(fortune-teller)/shop/index.tsx
+++ app/(fortune-teller)/shop/index.tsx
@@ -2,6 +2,7 @@ import ScreenWrapper from "@/app/components/ScreenWrapper";
 import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import HeaderBar from "../../components/ui/HeaderBar";
 import { useRouter } from "expo-router";
+import { MaterialIcons } from "@expo/vector-icons"
 
 const products = [
   {
@@ -39,12 +40,67 @@ const products = [
 export default function ShopDashboardPage() {
   const router = useRouter();
 
-  return (
+    return (
     <ScreenWrapper>
       <HeaderBar
-        title="My Shop"
+        title="Fortune teller"
         showChat
       />
+      <ScrollView 
+        contentContainerStyle={{ 
+          paddingHorizontal: 16, 
+          paddingBottom : 20,
+          paddingTop:8
+        }}
+      >
+      <View className="flex-1">
+
+        {/* เมนูปุ่มกด */}
+        <TouchableOpacity
+          className="bg-primary-100 flex-row items-center justify-between rounded-full px-5 py-4 mb-4"
+          onPress={() => router.push("/(fortune-teller)/shop/create-new-product")}
+        >
+          <View className="flex-row items-center">
+            <MaterialIcons name="add-box" size={20} color="white" />
+            <Text className="text-alabaster text-base font-semibold ml-3">Create new product</Text>
+          </View>
+          <MaterialIcons name="chevron-right" size={22} color="white" />
+        </TouchableOpacity>
+
+        {/* แสดงรายการสินค้า */}
+        <View className="space-y-3">
+          {products.map((product) => (
+            <TouchableOpacity
+              key={product.id}
+              className="bg-primary-100 rounded-2xl flex-row p-2 mb-2"
+              onPress={() => router.push(`/(fortune-teller)/shop/${product.id}`)}
+            >
+              {/* รูปสินค้า */}
+              <Image
+                source={{ uri: product.image }}
+                className="w-24 h-24 rounded-xl"
+                resizeMode="cover"
+              />
+              
+              {/* ข้อมูลสินค้า */}
+              <View className="flex-1 ml-4 justify-center">
+                <Text
+                  className="text-alabaster font-medium"
+                  numberOfLines={2}
+                >
+                  {product.name}
+                </Text>
+                <Text className="text-yellow-400 text-base font-bold mt-2">
+                  ฿{product.price.toFixed(2)}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </View>
+
+       
+      </View>
+      </ScrollView>
     </ScreenWrapper>
-  );
+  )
 }
